339fbf8915bd5525f0737bfce6dee1236935db4f
guchengxi1994 <guchengxi1994@users.noreply.github.com>
2021-07-03 09:07:19 +0800

    flutter init

---
+++ frontend/freeit/android/app/src/main/java/com/xiaoshuyui/freeit/MainActivity.java
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: xiaoshuyui
+ * @email: guchengxi1994@qq.com
+ * @Date: 2021-07-03 08:56:47
+ * @LastEditors: xiaoshuyui
+ * @LastEditTime: 2021-07-03 09:01:38
+ */
+package com.xiaoshuyui.freeit;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ frontend/freeit/lib/pages/classify/classify_view.dart
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: xiaoshuyui
+ * @email: guchengxi1994@qq.com
+ * @Date: 2021-07-03 09:05:35
+ * @LastEditors: xiaoshuyui
+ * @LastEditTime: 2021-07-03 09:05:35
+ */
